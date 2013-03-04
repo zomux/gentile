c3d03f9376eac0fb4cb1c8f75817bfac8c439dfe
zomux <raphael@uaca.com>
2013-03-04 13:07:32 +0900

    Changed some parameter

--- MertScripts/tuning.sh
+++ MertScripts/tuning.sh
@@ -5,6 +5,6 @@ mincosts="-10 -10 -10 -10 -10 -10 -10 -10 -10"
 maxcosts="10 10 10 10 10 10 10 10 10"
 initcosts="0.00596950305498102 0.0702448396785429 0.0544369594255997 0.135654037244263 0.157281527993101 -0.0436228192716449 -0.144734369757711 -0.024341215018477 0.363714728555679"
 ref="/poisson2/home2/raphael/ntcir10/data.dev.full/data.ja"
-workdir="/poisson2/home2/raphael/ntcir10/gentile.full.model/tuning"
+workdir="/now03/raphael/ntcir10/gentile.shorten.model/tuning"
 LC_ALL=C MertScripts/mert-1.33/mert.rb --source X --reference "$ref" --workdir "$workdir" --command "$command" --min "$mincosts" --max "$maxcosts" --ini "$initcosts"
 
